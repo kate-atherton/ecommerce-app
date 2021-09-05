d18ee2fb181eb11e388280658c248dd6b6dc5026
kate Atherton <kateatherton222@gmail.com>
2021-09-05 13:28:15 +0100

    first commit to begin project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import cart from "./cart.svg";
+import "./Sass/Base.scss";
+import Item from "./Item";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Main">
+      <header className="header">
+        <img src={cart} className="header__logo" alt="logo" />
       </header>
+      <section>
+        <h1>Products</h1>
+        <Item></Item>
+      </section>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,11 @@
+// import { product } from "prelude-ls";
+import React from "react";
+
+export default function Item() {
+  return (
+    <div className="product">
+      <img src="" alt=""></img>
+      <p className="description"></p>
+    </div>
+  );
+}
